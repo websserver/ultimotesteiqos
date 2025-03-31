e7bac2da4ec4790a7c4e509d932638f6fbc44a72
websserver <websserver2@gmail.com>
2025-03-31 14:52:43 +0100

    Adicionada mensagem quando o usuário perde o marcador Hiro

--- js/ilumai.js
+++ js/ilumai.js
@@ -32,10 +32,10 @@ document.addEventListener('DOMContentLoaded', function() {
   // Esconder loading após 5 segundos (fallback)
   setTimeout(hideLoading, 5000);
 
-const element = document.getElementsByClassName("back-button");
-element.addEventListener("click", function() {
-  alert("teste");
-});
+  const element = document.getElementsByClassName("back-button")[0];
+  element.addEventListener("click", function() {
+    alert("teste");
+  });
 
 
 });
