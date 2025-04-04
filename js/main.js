8d7d047ed600a2b9065d7ed1209c38f340d6f33d
websserver <websserver2@gmail.com>
2025-04-04 18:39:30 +0100

    Atualização do arquivo main.js

--- js/main.js
+++ js/main.js
@@ -113,7 +113,7 @@ function showModelInfo(modelId) {
 
     modelInfo.innerHTML = `
         <h2>${modelConfig.name}</h2>
-        <button onclick="window.location.href='${modelConfig.link}'">Personalizar</button>
+        <button class="pulse-button" onclick="window.location.href='${modelConfig.link}'">Personalizar</button>
     `;
     modelInfo.style.display = 'flex';
 }
